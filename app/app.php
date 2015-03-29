a4477655915f00ecf6d028bd3e6e714af0ab51e6
tommy bountham <bvone@ymail.com>
2015-03-28 20:32:30 -0700

    add

--- app/app.php
+++ app/app.php
@@ -13,27 +13,21 @@
 
 
     $app->get("/", function() use ($app) {
-        return $app['twig']->render('index.html.twig');
-    });
-
-    $app->delete('/', function() use ($app) {
-
-        if(!empty($_POST['stores'])) {
-            Store::deleteAll();
-        }
-        else {
-            Brand::deleteAll();
-        }
-
-        return $app['twig']->render('index.html.twig', array('newStore' => Store::getAll()));
+        return $app['twig']->render('index.html.twig', array('newBrand' => Brand::getAll(),'newStore' => Store::getAll()));
     });
 
 
     $app->post("/stores", function() use ($app) {
       $store_name = new Store($_POST['add_store']);
       $store_name->save();
-      return $app['twig']->render('index.html.twig', array('newStore' => Store::getAll()));
-    });
+      return $app['twig']->render('index.html.twig', array('newBrand' => Brand::getAll(),'newStore' => Store::getAll()));
+      });
+
+        $app->post("/brands", function() use ($app) {
+          $store_brand = new Brand($_POST['add_brand']);
+          $store_brand->save();
+          return $app['twig']->render('index.html.twig', array('newBrand' => Brand::getAll(),'newStore' => Store::getAll()));
+        });
   return $app;
 
 
